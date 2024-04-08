3382c71995bea135faceea57b526eadf878570d8
nguyễn thái nhân <ntnhan90@gmail.com>
2024-04-08 12:49:10 +0700

    update version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserModule } from './modules/user/user.module';
+import { ProductModule } from './modules/product/product.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: '',
+            port: 8889,
+            username: 'root',
+            password: 'root',
+            database: '',
+            entities: [],
+            synchronize: false,
+            autoLoadEntities: true,
+        }),
+        UserModule,
+        ProductModule,
+        AuthModule;
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
