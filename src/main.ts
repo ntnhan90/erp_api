8ff51f6de94eb88b69108cc2441e1842ad7a926a
nguyễn thái nhân <ntnhan90@gmail.com>
2024-04-08 13:18:33 +0700

    update modules

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configSwagger } from '@configs/api-docs.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  configSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
